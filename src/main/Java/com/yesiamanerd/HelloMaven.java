75c13708e8d1dab40854d3087c436cb5b68990d1
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-05 18:02:18 -0500

    Completed the logback file for the formatted STDOUT logger

--- src/main/Java/com/yesiamanerd/HelloMaven.java
+++ src/main/Java/com/yesiamanerd/HelloMaven.java
@@ -1,7 +1,16 @@
 package com.yesiamanerd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 public class HelloMaven {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        LOGGER.info("Hello Info");
+        LOGGER.debug("Hello Debug");
     }
 }
